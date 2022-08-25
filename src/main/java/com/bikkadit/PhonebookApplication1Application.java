60b0d7e09418af6967d3b35293ba65f23e7818d4
madangade11 <madangade7@gmail.com>
2022-08-25 19:54:05 +0530

    :phon-2

---
+++ src/main/java/com/bikkadit/PhonebookApplication1Application.java
@@ -0,0 +1,13 @@
+package com.bikkadit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookApplication1Application.class, args);
+	}
+
+}
